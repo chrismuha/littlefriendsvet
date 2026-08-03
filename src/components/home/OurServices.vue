e1bb596eacb3578cc455e0ae2c3f95b863bfa19a
Chris Muha <muhachris297@gmail.com>
2026-08-03 09:26:30 -0400

    fix: refine homepage doctor image and CTA

--- src/components/home/OurServices.vue
+++ src/components/home/OurServices.vue
@@ -9,7 +9,7 @@
             </div>
         </div>
         <div class="row orange-bg">
-            <a href="https://littlefriendsvetservices.securevetsource.com/index.pml"><h1>Click here for our online store <br>home delivery of your pet's medications AND food!</h1></a>
+            <a href="https://littlefriendsvetservices.securevetsource.com/index.pml"><h1><span class="cta-lead">Click here</span> for our online store <br>home delivery of your pet's medications AND food!</h1></a>
             <div class="image-wrapper">
                 <img src="@/assets/images/Businesscard.image4-f1df1fbc.jpg" alt="Businesscard Image">
             </div>
@@ -36,10 +36,20 @@
 .row.orange-bg a {
     color: white;
 }
-.row.orange-bg a:hover {
+.cta-lead {
     text-decoration: underline;
     text-decoration-thickness: 3px;
     text-underline-offset: 6px;
+}
+.row.orange-bg a:hover h1,
+.row.orange-bg a:focus-visible h1 {
+    text-decoration: underline;
+    text-decoration-thickness: 3px;
+    text-underline-offset: 6px;
+}
+.row.orange-bg a:hover .cta-lead,
+.row.orange-bg a:focus-visible .cta-lead {
+    text-decoration: none;
 }
 .image-wrapper {
     height: 300px;
