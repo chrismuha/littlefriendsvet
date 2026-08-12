19cbc6866d01c91577b09cd984b71959eb3223a9
Chris Muha <muhachris297@gmail.com>
2026-08-12 15:00:42 -0400

    legacy file.

--- src/components/home/ServicesWeOffer.vue
+++ src/components/home/ServicesWeOffer.vue
@@ -8,75 +8,6 @@ import newPetInformationForm from '@/assets/forms/LFVS-NewPet-Information.pdf'
         <img src="@/assets/images/Starburst_group_left_Orange-Teal.png" alt="" class="starburst">
         <div class="content-container">
             <div class="left-section">
-                <div class="section-item">
-                    <h1>Services We Offer</h1>
-                    <div class="text-wrapper">
-                        <ul>
-                            <li>Care for pets of all ages – from new pet care to senior pet care.</li>
-
-                            <li>We offer care for cats, dogs, small mammals, birds and reptiles – even fish!</li>
-
-                            <li>
-                                Preventative health care including:
-                                <ul>
-                                    <li>Examinations – annual wellness exams or if your pet is not feeling well</li>
-
-                                    <li>
-                                        Vaccinations including Rabies and Distemper for both dogs and cats; Lyme,
-                                        Leptosporosis and Kennel cough for your dog; and Feline Leukemia for your cat
-                                    </li>
-
-                                    <li>
-                                        Parasite prevention including flea and tick preventatives for your dog and cat;
-                                        and heartworm preventatives for your dog
-                                    </li>
-
-                                    <li>
-                                        Laboratory testing including stool and urine testing, heartworm and Lyme testing,
-                                        feline leukemia/FIV testing, and all types of blood tests.
-                                    </li>
-                                </ul>
-                            </li>
-
-                            <li>
-                                Care for new pets, like puppies and kittens to provide a life-long plan of health and
-                                wellness
-                            </li>
-
-                            <li>
-                                Care for senior pets with a focus on prevention and treatment of disease common to older
-                                pets
-                            </li>
-
-                            <li>
-                                Diagnosis and management of chronic health conditions such as diabetes, arthritis, skin
-                                conditions and more.
-                            </li>
-
-                            <li>
-                                Special focus on feline behavior issues – we offer behavioral consultations for cats for
-                                problems such as inappropriate elimination (not using the litter box) and aggression.
-                            </li>
-
-                            <li>Training and behavior consultations for dogs and puppies.</li>
-
-                            <li>
-                                Grooming services for cats including bathing, brushing, nail trims, sanitary clips and
-                                full lion cut clips.
-                            </li>
-
-                            <li>End of life care.</li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="section-item">
-                    <h1>Surgical Services</h1>
-                    <div class="text-wrapper">
-                        <p>
-                            We offer surgical services for cats including spaying and neutering, as well as more advanced surgery and all dental services for cats. We provide all health care for your dog from puppies to seniors, however we do not provide routine surgical services for dogs.  Please contact us with any questions.
-                        </p>
-                    </div>
-                </div>
                 <div class="section-item emergency-services">
                     <h1>Emergency Services</h1>
                     <div class="text-wrapper">
@@ -110,17 +41,6 @@ import newPetInformationForm from '@/assets/forms/LFVS-NewPet-Information.pdf'
                                 <a href="tel:+13155153047">315-515-3047</a>. Services and hours limited, please check their website or call them.
                             </li>
                         </ul>
-                    </div>
-                </div>
-                <div class="section-item">
-                    <h1>Spay Neuter Now</h1>
-                    <div class="text-wrapper">
-                        <p>
-                            Dr. Underwood partners with <b>Spay Neuter Now</b> to offer low cost spay and neuter services for cats to income qualified families. For cats only, we offer this service for qualified families right at our office. For dogs, Town and Country Veterinary Clinic in Massena offers this service to qualified families. The application must be submitted to Spay Neuter Now and qualified families will be contacted to schedule their pets. Click on the link for more information and to print or submit the application.
-                        </p>
-                        <p>
-                            <a href="https://www.spayneuternow.org/" class="btn btn-orange">Learn More About Spay Neuter Now</a>
-                        </p>
                     </div>
                 </div>
                 <div id="new-client" class="section-item new-client-section">
@@ -189,11 +109,6 @@ section {
     height: 150px;
     opacity: 50%;
     z-index: -1;
-}
-.btn {
-    font-size: 14px;
-    padding: 0 30px;
-    line-height: 38px;
 }
 .section-item h1 {
     font-family: 'Galada';
